ffc8e27a0b19e4c85f64e183d748e7165707a552
Dyllon <dyllontan1006@gmail.com>
2025-01-07 15:47:02 +0800

    portal done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'start-page';
+
+  constructor(private router:Router){
+  }
+
+  public GotoQUT(){
+    window.location.href = 'https://qutvirtual4.qut.edu.au/group/student/home'
+  }
+
+  public GotoGithub(){
+    window.location.href = 'https://github.com'
+  }
+
+  public GotoGoogle(){
+    window.location.href = 'https://www.google.com'
+  }
 }
+
